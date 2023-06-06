2a988e1590ccf05758233195d04e6f4e6db9e1e7
julia sokova <jlia_1990@mail.ru>
2023-06-07 02:30:30 +0300

    Initial commit

--- src/test/java/ru/netology/services/RecreationTest.java
+++ src/test/java/ru/netology/services/RecreationTest.java
@@ -2,26 +2,33 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RecreationTest {
-
-    @Test
-    public void testCountRestMonthsCondition1() {
-       Recreation recreation = new Recreation();
-
-       int actual = recreation.calculate( 10000, 3000, 20000);
-       int expected = 3;
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testCountRestMonthsCondition2() {
+    @ParameterizedTest
+    //  @CsvSource({
+    //         "3,10000,3000,20000",
+    //         "2,100000,60000,150000"
+    //  })
+    @CsvFileSource(files = "src/test/resources/recreation.csv")
+    public void testCountRestMonthsCondition1(int expected, int income, int expenses, int threshold) {
         Recreation recreation = new Recreation();
 
-        int actual = recreation.calculate( 100000, 60000, 150000);
-        int expected = 2;
+        int actual = recreation.calculate(income, expenses, threshold);
+        //int expected = 3;
 
         Assertions.assertEquals(expected, actual);
     }
+
+    //   @Test
+    //public void testCountRestMonthsCondition2() {
+    // Recreation recreation = new Recreation();
+
+    //int actual = recreation.calculate( 100000, 60000, 150000);
+    // int expected = 2;
+
+    // Assertions.assertEquals(expected, actual);
+    // }
 }
